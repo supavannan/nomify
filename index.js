ee4e4ce9db94c614bd4a6dac6b742c8133796ef0
supavannan <supavannan@gmail.com>
2021-05-07 21:02:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send({ hi: "there" }));
+
+//if heroku pushes env variables, otherwise use 5000 (dev)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+// if (process.env.NODE_ENV === "production") {
+//   //make sure Express will serve up production assets
+//   app.use(express.static("client/build"));
+//   //serve index.js if route unrecognized
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", index.html))
+//   );
+// }
