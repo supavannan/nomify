277c9454a3de2a5770212846804d9a239b6f45d2
supavannan <supavannan@gmail.com>
2021-05-08 01:54:33 -0400

    trying to fix internal server error

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
-
-//266333193011-q8nd61qsvlmf8gqeu1fpgafcqcc17onl.apps.googleusercontent.com
-//Wm2qbCK45FObwN5M8VGH5XYZ
